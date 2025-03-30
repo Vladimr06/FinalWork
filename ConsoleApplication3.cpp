6f3c9487c8649e123fea50dd9af52626cad51063
Vladimir <cherdakovvladimir9@gmail.com>
2025-03-30 15:34:55 +0300

    Разделение объявление переменных и добавление комментариев

--- ConsoleApplication3.cpp
+++ ConsoleApplication3.cpp
@@ -6,7 +6,10 @@
 int main() {
 	setlocale(LC_ALL, "rus");
 
-	double x1, e1, y_prev, y_curr;
+	double x1; // первая переменная x1
+	double e1; // вторая e1
+	double y_prev1; // третья переменная
+	double y_curr1; // четвертая 
 
 	// Ввод исходных данных
 	printf("Введите значение x: ");
@@ -15,23 +18,23 @@ int main() {
 	scanf("%lf", &e1);
 
 	// Инициализация первого члена последовательности
-	y_prev = x1; // Начальное значение
-	y_curr = 0;
+	y_prev1 = x1; // Начальное значение
+	y_curr1 = 0;
 
 	int i = 1;
 
 	// Условие завершения
 	do {
 		// Вычисление следующего члена последовательности
-		y_curr = 0.5 * (y_prev + (x1 / y_prev) - 1);
+		y_curr = 0.5 * (y_prev1 + (x1 / y_prev1) - 1);
 
 		// Обновление предыдущего числа
 		i++;
-	} while (fabs(y_curr * y_curr - y_prev * y_prev) < e1);
+	} while (fabs(y_curr1 * y_curr1 - y_prev1 * y_prev1) < e1);
 	// Проверка условия завершения в while
 
 	// Вывод результата
-	printf("Первый член yn: %.10f\n", y_curr);
+	printf("Первый член yn: %.10f\n", y_curr1);
 
 	return 0;
 }
